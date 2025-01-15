98848b2b92ed38eaa8dcacab8afd6c0fce496d3c
NunTheeKnee <jane6677doe@gmail.com>
2025-01-15 16:24:52 +0530

    Added the main function

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const OFFER_ENDS_AT= new Date("2025-01-14 17:00:00 +0530") 
+const OFFER_ENDS_AT = new Date("2025-01-15T19:00:00+05:30");
+
 const OfferTimeShow = () => {
-    var CURRENT_DATE = new Date().toLocaleDateString();
-    var CURRENT_TIME = new Date().toLocaleTimeString();
-    var OFFER_ENDS_AT_DATE = OFFER_ENDS_AT.toLocaleDateString();
-    if (OFFER_ENDS_AT_DATE > CURRENT_DATE)
-        console.log("Offer ends in more than 24 hours")
-    else if(OFFER_ENDS_AT_DATE == CURRENT_DATE)
-        console.log("Offer ends in less than 24 hours")
-    else
-        console.log("Offer has ended")
-}
-OfferTimeShow()
+    const CURRENT_DATE_TIME = new Date();
+
+    const timeDifference = OFFER_ENDS_AT - CURRENT_DATE_TIME;
+    if (timeDifference > 0) {
+        const hoursRemaining = Math.floor(timeDifference / (1000 * 60 * 60));
+        const daysRemaining = Math.floor(hoursRemaining / 24);
+
+        if (daysRemaining > 0) {
+            console.log(`Offer ends in more than ${daysRemaining} days (${hoursRemaining} hours).`);
+        } else {
+            console.log(`Offer ends in less than 24 hours (${hoursRemaining} hours).`);
+        }
+    } else {
+        console.log("Offer has ended.");
+    }
+};
+
+OfferTimeShow();
